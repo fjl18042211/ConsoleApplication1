1d0eea1b2f54fbb98766d88d60eebb6825eda8ac
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-02-21 19:18:48 +0800

    printPrimeNumber

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,11 +2,35 @@
 //
 
 #include <iostream>
-
+#include <stdlib.h>
+int checkPrimeNumber(int n) {
+	int i, flag=0;
+	for (i = 2; i < n/2; i++)
+	{
+		if (n%i == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1) return 0;
+	else return 1;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i;
+	int totalNum = 100;
+	std::cout << "the number is :" << 2;
+	for (i = 3; i < totalNum; i = i + 2) {
+		int res = checkPrimeNumber(i);
+		if (res == 1)
+			std::cout << "the number is :" << i ;
+
+	}
+
+    
 }
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
